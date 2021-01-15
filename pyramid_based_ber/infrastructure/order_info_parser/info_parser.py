1a3a66cce66a37a3c10bf4a0ce0d1050402c7f30
VladSavitskiy <cyberl1fe@yandex.ru>
2021-01-16 00:06:50 +0500

    Добавлена валидация ингредиентов при заказе

--- pyramid_based_ber/infrastructure/order_info_parser/info_parser.py
+++ pyramid_based_ber/infrastructure/order_info_parser/info_parser.py
@@ -1,27 +1,52 @@
 from datetime import datetime
 
+from pyramid_based_ber.classes.burger_config.bun import Bun
+from pyramid_based_ber.classes.burger_config.cutlet import Cutlet
+from pyramid_based_ber.classes.burger_config.sauce import Sauce
 from pyramid_based_ber.classes.burger_config.sauces import Sauces
+from pyramid_based_ber.classes.burger_config.stuffing import Stuffing
 from pyramid_based_ber.classes.burger_config.stuffings import Stuffings
 from pyramid_based_ber.infrastructure.enums_parser import ingredients_parser
 from pyramid_based_ber.classes.burger import Burger
-from pyramid_based_ber.classes.order_dto import OrderDto
+
+from pyramid_based_ber.infrastructure.validation.info_validator import *
+
+default_values = {'bread': Bun.no_sesame, 'meat': Cutlet.beef,
+                  'sauces': [Sauce.ketchup, Sauce.mustard],
+                  'stuffings': [Stuffing.tomato, Stuffing.cucumber, Stuffing.cheese]}
 
 
 def parse_order_info(params) -> OrderDto:
     burger = parse_burger_info(params)
     return OrderDto(
         creation_date=datetime.now(tz=None),
-        name=remove_unnecessary_whitespaces(params['name']),
+        name= remove_unnecessary_whitespaces(params['name']),
         phone_number=remove_unnecessary_whitespaces(params['phone_number']),
         address=remove_unnecessary_whitespaces(params['address']),
         burger_config=burger)
 
 
 def parse_burger_info(params) -> Burger:
-    bun = ingredients_parser.parse_bun(params['bread'])
-    cutlet = ingredients_parser.parse_cutlet(params['meat'])
-    sauces = Sauces(ingredients_parser.parse_sauces(params.getall('sauce')))
-    stuffings = Stuffings(ingredients_parser.parse_stuffings(params.getall('stuffing')))
+    if 'bread' in params:
+        bun = ingredients_parser.parse_bun(params['bread'])
+    else:
+        bun = default_values['bread']
+
+    if 'meat' in params:
+        cutlet = ingredients_parser.parse_cutlet(params['meat'])
+    else:
+        cutlet = default_values['meat']
+
+    if 'sauce' in params:
+        sauces = Sauces(ingredients_parser.parse_sauces(params.getall('sauce')))
+    else:
+        sauces = Sauces(default_values['sauces'])
+
+    if 'stuffing' in params:
+        stuffings = Stuffings(ingredients_parser.parse_stuffings(params.getall('stuffing')))
+    else:
+        stuffings = Stuffings(default_values['stuffings'])
+
     return Burger(bun, cutlet, sauces, stuffings)
 
 
--- pyramid_based_ber/views/order_manager.py
+++ pyramid_based_ber/views/order_manager.py
@@ -1,15 +1,11 @@
 from pyramid.view import view_config
 from pyramid_based_ber.infrastructure.order_info_parser.info_parser import parse_order_info
 from pyramid_based_ber.infrastructure.order_manager.order_manager import register_order
-from pyramid_based_ber.infrastructure.validation.info_validator import validate
 
 
 @view_config(route_name='order', renderer='../templates/order_info.jinja2')
 def make_order(request):
     order_info = parse_order_info(request.params)
-    # if validate(order_info):
     order_id = register_order(request, order_info)
     order_info.order_id = order_id
     return {'order_info': order_info}
-    # else:
-    # return {'': 'Incorrect data'}
